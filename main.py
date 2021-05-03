2e479c00fdb965bcdb9a11e58d99f94dc2da867d
Gordep <jgonzalez107@miners.utep.edu>
2021-05-03 13:43:19 -0600

    added support for value Iteration and choosing with algorithm to use

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import Monte_Carlo
+import Value_Iteration
+
 
 class main():
     def __init__(self):
-        # run monte carlo
-        mc = Monte_Carlo.Monte_Carlo()
-        for x in range(50):
-            print("Episode ",x)
-            mc.execute()
         
-        print("Val for all states is" )
-        print(mc.MDP.get_States())
+        print("Choose algorithm")
+        print("1. Monete Carlo")
+        print("2. Value Iteration")
+        print("3. Q-Learning")
+        inputVal = input("> ")
+        
+        if inputVal == "1":
+        
+            # run monte carlo
+            mc = Monte_Carlo.Monte_Carlo()
+            for x in range(50):
+                print("Episode ",x)
+                mc.execute()
             
+            print("Val for all states is" )
+            print(mc.MDP.get_States())
+            
+        elif inputVal == "2":     
+            val_iter = Value_Iteration.Value_Iteration()
+            
+            val_iter.simulate()
+
+        elif inputVal == "3":
+            pass
+        
+        else:
+            print("invalid input")
             
 if __name__ == '__main__':
     main()
